3a61167f1aa2eb774ca0a8eba205e84d6e92046d
Eugene Croquette <croqo@outlook.com>
2021-05-05 17:28:18 +0300

    style.sass
    + import vendor styles
    + hero-head padding to prevent overlapping by nav

--- _source/index.js
+++ _source/index.js
@@ -1,6 +1,15 @@
-import style from 'bulma';
+import style from './style.sass';
 
 import { library, icon } from '@fortawesome/fontawesome-svg-core';
 import * as ico from '@fortawesome/free-solid-svg-icons';
 
-console.log(ico);
+
+$(()=>{
+   let h = $("nav").height();
+   $("header").each((index,item)=>{
+       $(item).css({
+        "padding-top": (h+30)
+        })
+   })
+});
+console.log('ok');
