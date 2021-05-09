8a9288d7fdf38465fb5d8c65e9a16e58aade6ab8
Eugene Croquette <croqo@outlook.com>
2021-05-09 09:03:29 +0300

    fullpage.js

--- _source/index.js
+++ _source/index.js
@@ -1,14 +1,38 @@
 import { library, icon } from '@fortawesome/fontawesome-svg-core';
 import * as ico from '@fortawesome/free-solid-svg-icons';
+import fullpage from 'fullpage.js';
 
 
 $(()=>{
+  initPaddingTop();
+  initTables();
+
+  new fullpage('#fullpage', {
+    licenseKey: "",
+    autoScrolling: true,
+    afterLoad: function(origin, destination, direction){
+      console.log([origin, destination, direction])
+    }
+  });
+
+});
+
+function initPaddingTop(){
   let h = $("nav").height();
   $(".hero-head").each((index,item)=>{
     $(item).css({
     "padding-top": h
-    });
-    console.log(`Head height: ${h}`);
+    })
+  })
+}
+function initTables(){
+  $("table").each((index, item)=>{
+    $(item).addClass("container");
   });
-
-});
+  $("tr").each((index, item)=>{
+    $(item).addClass("columns");
+  });
+  $("td").each((index, item)=>{
+    $(item).addClass("column");
+  });
+}
