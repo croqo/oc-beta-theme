bb1ac0f2fc32858aee74e8dd3d728983510df194
Croqo <croqo@outlook.com>
2023-07-25 04:55:02 +0000

    use turbo router API

--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('page:loaded', () => {
 
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
