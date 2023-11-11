2b17b8f28905aff00d6a12da739ac18d573a4948
Eugene Croquette <croqo@outlook.com>
2023-11-11 01:48:19 +0000

    tip from docs

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ const webpack = require('webpack');
 
 module.exports = {
     devtool: 'inline-source-map',
-    externals: { oc: 'octobercms'},
+    // webpack recommendations
+    // https://docs.octobercms.com/3.x/cms/ajax/turbo-router.html#working-with-hot-reloading
+    snippetOptions: {
+        rule: {
+            match: /<\/head>/i,
+            fn: function (snippet, match) {
+                return '<meta name="turbo-visit-control" content="disable" />';
+            }
+       }
+    }
 };
